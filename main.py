d260bef4ed987e7a003fed2a2fc29dbb65f6305c
cokoroafor11 <cokoroafor@gmail.com>
2025-10-22 10:16:12 -0400

    Adding dynmaic chat features, error handling

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
+
 
 def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     print("Hello from ai-code-agent!")
+    script_name = sys.argv[0]
+    if '-v' in sys.argv or '--verbose' in sys.argv:
+        verbose_mode = True
+    else:
+        verbose_mode = False
+    if len(sys.argv) > 1:
+        user_prompt = " ".join(sys.argv[1:])
+    else:
+        print("Error: No prompt entered")
+        sys.exit(1)
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
     response = client.models.generate_content(
-    model='gemini-2.0-flash', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.""Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model='gemini-2.0-flash', contents=messages
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
+    if verbose_mode == True:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+ 
 
 if __name__ == "__main__":
     main()
